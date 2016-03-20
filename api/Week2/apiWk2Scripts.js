11784e4517b1e9cfc79f08ccce2efe78e6073ef8
lpaglione <l.paglione@orcid.org>
2016-03-20 19:19:00 -0400

    APO Week 2 homework

---
+++ api/Week2/apiWk2Scripts.js
@@ -0,0 +1,10 @@
+// --------------------------------------
+// Laura Paglione (MENTOR)            
+// NanoHacker Academy                 
+// Spring 2016: Got Game (JavaScript) 
+//
+// apiWk2Scripts.js
+// March 13, 2016                      
+// --------------------------------------
+
+// Scripts for the adventure game
